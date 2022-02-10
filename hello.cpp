9bccb0f7fe798f0b9578dfda19a128c9e1d265d8
ayelo-m <ayeloebeli@gmail.com>
2022-02-10 20:43:27 +0200

    assignment3-1.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+using namespace std; 
+
 int main()
 {
-     std::cout << "Hello World" << std::endl;
+	char c1 = 'A';
+	char c2 = 'B';
+
+
+	int c1_ascii;
+	c1_ascii = (int)c1;
+	cout << "ASCII code for c1 " << c1_ascii << endl;
+	int c2_ascii;
+	c2_ascii = (int)c2;
+	cout << "ASCII code for c2 " << c2_ascii << endl;
+
 }
